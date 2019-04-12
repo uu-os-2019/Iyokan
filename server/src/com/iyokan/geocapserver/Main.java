a4c0229728c3a77fdd13886d1a6ad271564ebdae
Joakim Michalak <joakim.michalak@gmail.com>
2019-04-12 14:46:30 +0200

    Highscore almost done

--- server/src/com/iyokan/geocapserver/Main.java
+++ server/src/com/iyokan/geocapserver/Main.java
@@ -6,13 +6,18 @@ public class Main {
 
     public static void main(String[] args) {
         final int port = 80;
+        Highscore hs = new Highscore();
 
         final Route[] routes = new Route[] {
-                new RouteTest()
+                new RouteTest(),
+                new RouteHighscore(hs)
         };
+
 
         Server server = new Server(port, routes);
 
         server.start();
+        System.out.println(hs.getHighscore(3));
+
     }
 }
