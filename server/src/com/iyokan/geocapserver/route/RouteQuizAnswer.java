a387f5113f82a3c03cc2e88bc5b8f378a5bb9133
Joakim Michalak <joakim.michalak@gmail.com>
2019-04-24 11:36:35 +0200

    POST request instead of query

--- server/src/com/iyokan/geocapserver/route/RouteQuizAnswer.java
+++ server/src/com/iyokan/geocapserver/route/RouteQuizAnswer.java
@@ -15,30 +15,17 @@ public class RouteQuizAnswer extends Route {
 
     public RouteQuizAnswer(QuizRoundCollection quizRoundCollection){this.quizRoundCollection = quizRoundCollection;}
 
-    //Tar http GET strängen från URLen och mappar ut alla variabler
-    //T.ex. queryToMap("?question=när?&answer=100") -> Map(question, när)..etc
-    //TODO flytta funktionen till rätt ställe, lär behövas på andra ställen
-    public Map<String, String> queryToMap(String query) {
-        Map<String, String> result = new HashMap<>();
-        for (String param : query.split("&")) {
-            String[] entry = param.split("=");
-            if (entry.length > 1) {
-                result.put(entry[0], entry[1]);
-            }else{
-                result.put(entry[0], "");
-            }
-        }
-        return result;
-    }
 
     public JSONObject handle(RequestData data) {
+        JSONObject json = data.getJSON();
+
         JSONObject response = new JSONObject();
 
         response.put("type", "quiz_answer");
         String request = data.getRequest();
-        Map<String, String> params = queryToMap(request); //TODO: nullpointer exception handling if query is null
-        String question = params.get("question");
-        String answer = params.get("answer");
+
+        String question = json.getString("question");
+        String answer = json.getString("answer");
 
         //Hittar rätt quizRound i collections och kontrollerar spelar svaret och sätter bool i JSON
         QuizRound[] quizRounds = quizRoundCollection.getAllQuizRounds();
