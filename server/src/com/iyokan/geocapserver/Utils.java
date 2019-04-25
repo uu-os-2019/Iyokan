7fcec9f4ebfee6c3463869d2f92bff4b9440b6a1
Anton <noten.b@hotmail.com>
2019-04-25 10:23:45 +0200

    Clamped possible score

--- server/src/com/iyokan/geocapserver/Utils.java
+++ server/src/com/iyokan/geocapserver/Utils.java
@@ -6,6 +6,28 @@ public class Utils {
 
     private static Random rnd = new Random();
 
+    /**
+     * Clamps value between min and max
+     * @param value
+     * @param min
+     * @param max
+     * @return
+     */
+    public static long clamp(long value, long min, long max) {
+        return Math.min(Math.max(value, min), max);
+    }
+
+    /**
+     * Clamps value between min and max
+     * @param value
+     * @param min
+     * @param max
+     * @return
+     */
+    public static double clamp(double value, double min, double max) {
+        return Math.min(Math.max(value, min), max);
+    }
+
     public static byte[] generateBytes(int length) {
         // TODO make this thread safe
         byte[] b = new byte[length];
