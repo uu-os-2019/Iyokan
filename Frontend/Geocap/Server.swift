09c4d8cfe74b90e646a9ecf2e856e581f72c1c6f
Oscar Englöf <oscar.englof@hotmail.com>
2019-05-06 11:12:27 +0200

    fix

--- Frontend/Geocap/Server.swift
+++ Frontend/Geocap/Server.swift
@@ -68,7 +68,7 @@ class Server {
     }
     
     func getLocations() -> [Location] {
-        let url = "http://130.243.234.130/location/get-all"
+        let url = "http://localhost/location/get-all"
         let urlObject = URL(string: url)!
         var locationsJSON: jsonLocations!
         let semaphore = DispatchSemaphore(value: 0) // Semaphore used for forcing dataTask to finish before returning
@@ -91,7 +91,7 @@ class Server {
     
     func getQuiz() -> Quiz? {
         var quiz: Quiz!
-        let url = URL(string: "http://130.243.234.130/quiz/start")!
+        let url = URL(string: "http://localhost/quiz/start")!
         var request = URLRequest(url: url)
         request.addValue("OsthyvelOsthyvelOsthyvelOsthyvel", forHTTPHeaderField: "Authorization")
         request.httpMethod = "POST"
@@ -126,7 +126,7 @@ class Server {
     
     func sendQuizAnswer(answer: String) -> QuizAnswer? {
         var quizAnswer: QuizAnswer!
-        let url = URL(string: "http://130.243.234.130/quiz/answer")!
+        let url = URL(string: "http://localhost/quiz/answer")!
         var request = URLRequest(url: url)
         request.addValue("OsthyvelOsthyvelOsthyvelOsthyvel", forHTTPHeaderField: "Authorization")
         request.httpMethod = "POST"
